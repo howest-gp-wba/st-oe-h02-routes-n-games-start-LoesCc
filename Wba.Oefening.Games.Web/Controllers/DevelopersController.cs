b3f99ac9f694b3a82b84cf1bab059d8377327b09
LoesCc <loes.coucke@student.howest.be>
2025-01-17 20:21:01 +0100

    Add DevelopersController with FormatDeveloperInfo(Developer)

---
+++ Wba.Oefening.Games.Web/Controllers/DevelopersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Wba.Oefening.Games.Core.Entities;
+
+namespace Wba.Oefening.Games.Web.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private string FormatDeveloperInfo(Developer developer)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<div>");
+            sb.AppendLine("<h3>Developer info</h3>");
+            sb.AppendLine("<ul>");
+            sb.AppendLine($"<li>Developer Id: {developer?.Id ?? 0}</li>");
+            sb.AppendLine($"<li>Name: {developer?.Name ?? "<unknown>"}</li>");
+            sb.AppendLine("</ul>");
+            sb.AppendLine("</div>");
+            return sb.ToString();
+        }
+    }
+}
